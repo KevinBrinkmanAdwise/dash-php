2a69fab59bb979ee7fd84935dea3170efb54fe21
KevinBrinkmanAdwise <k.brinkman@adwise.nl>
2018-10-12 13:07:18 +0200

    Include header & Footer

---
+++ inc/header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Import Source Sans -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+
+  <!-- Ionic Icons -->
+  <link href="https://unpkg.com/ionicons@4.4.4/dist/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- import custom stylesheet -->
+  <link rel="stylesheet" href="styles/css/styles.css">
+
+  <title>Dashboard | Adwise</title>
+</head>
+
+<body>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-2 px-1" id="sticky-sidebar">
+        <div class="sticky-top">
+          <nav class="logo">
+            <span class="navbar-brand mb-0 h1">Dashboard Name</span>
+          </nav>
+
+          <div class="nav flex-column">
+            <ul>
+              <li class="active">
+                <img src="img/menu_home.svg" style="width:50px;" alt="">
+                <a href="home.html" class="nav-link">Home</a>
+              </li>
+              <li>
+                <img src="img/menu_support.svg" style="width:50px;" alt="">
+                <a href="tickets.html" class="nav-link">Tickets</a>
+              </li>
+              <li>
+                <img src="img/menu_websites.svg" style="width:50px;" alt="">
+                <a href="websites.html" class="nav-link">Websites</a>
+              </li>
+              <li>
+                <img src="img/menu_klanten.svg" style="width:50px;" alt="">
+                <a href="klanten.html" class="nav-link">Klanten</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+
+      
